268081c0ff11b324d75a0b220e319e61064d6e94
LeDuoc95 <leduoc0804@gmail.com>
2020-10-15 17:40:13 +0700

    15_10_2020

--- asset/js/homePage.js
+++ asset/js/homePage.js
@@ -1,57 +1,56 @@
-const statisticalProject = document.querySelector('.statistical__project span');
-const statisticalClient = document.querySelector('.statistical__client span');
-const statisticalCup = document.querySelector('.statistical__cup span');
-const statisticalPerson = document.querySelector('.statistical__person span');
+const statisticalProject = document.querySelector(".statistical__project span");
+const statisticalClient = document.querySelector(".statistical__client span");
+const statisticalCup = document.querySelector(".statistical__cup span");
+const statisticalPerson = document.querySelector(".statistical__person span");
 let theFirst = true;
-
 
 if (theFirst && window.pageYOffset >= 1550) {
   theFirst = false;
-  for(let i = 0; i < 1750;i++) {
-    setTimeout( () => statisticalProject.innerHTML = i,600);
+  for (let i = 0; i < 1750; i++) {
+    setTimeout(() => (statisticalProject.innerHTML = i), 600);
   }
-  for(let i = 0; i < 6784;i++) {
-    setTimeout( () => statisticalClient.innerHTML = i,600);
+  for (let i = 0; i < 6784; i++) {
+    setTimeout(() => (statisticalClient.innerHTML = i), 600);
   }
-  for(let i = 0; i < 2536;i++) {
-    setTimeout( () => statisticalCup.innerHTML = i,600);
+  for (let i = 0; i < 2536; i++) {
+    setTimeout(() => (statisticalCup.innerHTML = i), 600);
   }
-  for(let i = 0; i < 444;i++) {
-    setTimeout( () => statisticalPerson.innerHTML = i,600);
+  for (let i = 0; i < 444; i++) {
+    setTimeout(() => (statisticalPerson.innerHTML = i), 600);
   }
 }
 
 const countNumber = () => {
-  console.log('object')
+  console.log("object");
   if (theFirst && window.pageYOffset >= 1550) {
     theFirst = false;
-    for(let i = 0; i < 1750;i++) {
-      setTimeout( () => statisticalProject.innerHTML = i,600);
+    for (let i = 0; i < 1750; i++) {
+      setTimeout(() => (statisticalProject.innerHTML = i), 600);
     }
-    for(let i = 0; i < 6784;i++) {
-      setTimeout( () => statisticalClient.innerHTML = i,600);
+    for (let i = 0; i < 6784; i++) {
+      setTimeout(() => (statisticalClient.innerHTML = i), 600);
     }
-    for(let i = 0; i < 2536;i++) {
-      setTimeout( () => statisticalCup.innerHTML = i,600);
+    for (let i = 0; i < 2536; i++) {
+      setTimeout(() => (statisticalCup.innerHTML = i), 600);
     }
-    for(let i = 0; i < 444;i++) {
-      setTimeout( () => statisticalPerson.innerHTML = i,600);
+    for (let i = 0; i < 444; i++) {
+      setTimeout(() => (statisticalPerson.innerHTML = i), 600);
     }
-	}
+  }
 };
 
-function openTab(evt, cityName) {
-  var i, tabcontent, tablinks;
+function openTab(evt, cityName, index) {
+  let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    tablinks[i].classList.remove("active");
   }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+  document.getElementById(cityName).style.display = "flex";
+  tablinks[index - 1].classList.add("active");
 }
 
-document.addEventListener('scroll', countNumber);
+document.addEventListener("scroll", countNumber);
